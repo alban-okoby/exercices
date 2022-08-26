1a700cd7e1c8e938b406679898e2eec2b8477e48
alban-okoby <albanokoby225@gmail.com>
2022-08-26 13:43:34 +0200

    add javadoc to Personnel  class on java-exercice EPFL

--- java/epflens/src/com/epfl/Personnel.java
+++ java/epflens/src/com/epfl/Personnel.java
@@ -4,6 +4,15 @@ public class Personnel extends EPFLien {
 	protected String laboratoire;
 	protected double salaire;
 	
+
+	/**
+	 * Constructeur de la class Personnel 
+	 * Sachant qu'un personnel est soit de type ( Enseignant ou Secretaire )
+	 * @param nom le nom du personnel
+	 * @param annee l'année de début de service 
+	 * @param laboratoire le laboratoire ou il exerce 
+	 * @param salaire le salaire du personnel
+	 */
 	public Personnel(String nom, int annee, String laboratoire, double salaire) {
 		super(nom, annee);
 		this.laboratoire = laboratoire;
