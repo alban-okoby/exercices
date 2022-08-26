3cdb5ae0a1c088d24eca539effa8267089aaa35e
alban-okoby <albanokoby225@gmail.com>
2022-08-26 13:13:30 +0200

    add javadoc to Etudiantregulier class on java-exercice EPFL

--- java/epflens/src/com/epfl/Direction.java
+++ java/epflens/src/com/epfl/Direction.java
@@ -12,13 +12,14 @@ public class Direction {
         EtudiantRegulier cecile = new EtudiantRegulier("Koffi Cecile", 2022, "UVCI", 7.3);
         EtudiantRegulier yvan = new EtudiantRegulier("Yvan Rattrapeur", 2011, "SSC", 2.5);
         EtudiantEchange bjorn = new EtudiantEchange("Bjorn Borgue", 2012, "Informatique", "KTH");
+        EtudiantEchange Arnaud = new EtudiantEchange("Yao Arnaud", 2005, " ", "PIGIER");
         EtudiantRegulier gaston = new EtudiantRegulier("Gaston Peutimide", 2013, "SSC", 6.0);
         
-        // Création de personnels ( Enseignant ou Secretaire )
+        // Création de personnel ( Enseignant ou Secretaire )
         Enseignant math = new Enseignant("Mathieu Matheu", 1998, "LMEP", 10000, "Physique");
         Secretaire sophie = new Secretaire("Sophie Scribona", 2005, "LMT", 5000);
 
-        // Instation d'objet Epfl pour y ajouter le Personnel crée
+        // Instation d'objet Epfl pour y ajouter le Personnel et/ou les Etudiants crées
         EPFL epfl = new EPFL();
         
         // Ajout des personnes instantées
@@ -27,7 +28,8 @@ public class Direction {
                 .ajouter(yvan)
                 .ajouter(bjorn)
                 .ajouter(math)
-                .ajouter(sophie);
+                .ajouter(sophie)
+                .ajouter(Arnaud);
         
         String str = "Parmi les " + epfl.totalEPFLien() + " EPFLiens, ";
         str += epfl.totalEtudiant() + " sont des etudiants.\n";
--- java/epflens/src/com/epfl/EPFL.java
+++ java/epflens/src/com/epfl/EPFL.java
@@ -11,22 +11,32 @@ public class EPFL {
 		epfliens = new ArrayList<>();
 	}
 	
+	/**
+	 * @param e
+	 * @return Ajouter un EPPLien ( soit un étudiant ou un personnel)
+	 */
 	public EPFL ajouter(EPFLien e) {
 		epfliens.add(e);
 		return this;
 	}
 	
-	public EPFLien get(int index) {
-		if (index < totalEPFLien()) {
-			return epfliens.get(index);
+	public EPFLien get(int i) {
+		if (i < totalEPFLien()) {
+			return epfliens.get(i);
 		}
 		return null;
 	}
 	
+	/**
+	 * @return Méthode qui renvoie le nombres d'EPFL (le nombre d'étudiants et de personnels)
+	 */
 	public int totalEPFLien() {
 		return epfliens.size();
 	}
 	
+	/**
+	 * @return Méthode qui renvoie le nombres d'EPFL (soit le nombre d'étudiants)
+	 */
 	public int totalEtudiant() {
 		int nbreEtudiant = 0;
 		for (EPFLien ep : epfliens) {
@@ -37,8 +47,12 @@ public class EPFL {
 		return nbreEtudiant;
 	}
 	
+	/**
+	 * @return Cette méthode retourne le nombre d'années moyen pendant lesquelles les personnes enregistrées ont fréquenté l'EPFL
+	 */
 	public double tempsMoyen() {
 		double moyenne = 0.;
+		// Affichage de l'année courante
 		int anneeCourante  = Calendar.getInstance().get(Calendar.YEAR);
 		for (EPFLien ep : epfliens) {
 			if (ep.estEtudiant()) {
@@ -49,8 +63,11 @@ public class EPFL {
 		return moyenne;
 	}
 	
+	/**
+	 * affiche la liste de tous les EPPLiens
+	 */
 	public void afficher() {
-		System.out.println("Liste des EPFLiens:");
+		System.out.println("La liste des EPFLiens:");
 		for (EPFLien ep : epfliens) {
 			ep.afficher();
 		}
